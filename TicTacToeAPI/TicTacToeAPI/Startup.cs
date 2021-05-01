771cbb24a8bb3bb48b464295f1b768189824d768
abhana <abhana@homechoice.co.za>
2021-05-01 10:44:10 +0200

    Add Swagger + startup config

--- TicTacToeAPI/TicTacToeAPI/Startup.cs
+++ TicTacToeAPI/TicTacToeAPI/Startup.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
 
 namespace TicTacToeAPI
 {
@@ -17,6 +14,16 @@ namespace TicTacToeAPI
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
+
+      var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Is(LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .Enrich.WithExceptionDetails()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+    .MinimumLevel.Override("System", LogEventLevel.Warning);
+
+      Log.Logger = loggerConfiguration.CreateLogger();
     }
 
     public IConfiguration Configuration { get; }
@@ -25,6 +32,9 @@ namespace TicTacToeAPI
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+
+      services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,7 +43,11 @@ namespace TicTacToeAPI
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicTacToe v1"));
       }
+
+      app.UseHttpsRedirection();
 
       app.UseRouting();
 
---
+++ TicTacToeAPI/TicTacToeAPI/StartupExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using TicTacToeAPI.Infrastructure.Config;
+
+namespace TicTacToeAPI
+{
+  public static class StartupExtensions
+  {
+    public static IApplicationBuilder UseCors(this IApplicationBuilder app, IConfiguration configuration)
+    {
+      var settings = configuration.GetSection(nameof(CORSSettings)).Get<CORSSettings>();
+
+      return app.UseCors(options =>
+      {
+        options
+            .WithHeaders(settings.AllowHeaders)
+            .WithMethods(settings.AllowMethods)
+            .WithOrigins(settings.AllowOrigins)
+            .WithExposedHeaders(settings.ExposedHeaders);
+
+        // Unable to allow credentials if the "*" wildcard is used for origins
+        if (!settings.AllowOrigins.Contains("*") && settings.AllowCredentials)
+        {
+          options.AllowCredentials();
+        }
+      });
+    }
+  }
+}
