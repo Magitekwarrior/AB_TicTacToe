a59fd0cce05bc76d37b68a7bc310533db7b2b5f8
abhana <abhana@homechoice.co.za>
2021-05-02 14:44:26 +0200

    modules added

--- TicTacToeApp/TicTacToeApp/src/app/app.module.ts
+++ TicTacToeApp/TicTacToeApp/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { GameService } from './game.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,11 @@ import { BoardComponent } from './board/board.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [GameService]
 })
 export class AppModule { }
