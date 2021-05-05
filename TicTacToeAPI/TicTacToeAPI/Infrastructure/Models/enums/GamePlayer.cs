f52933351335cc3dd3f035e0c73edf6b0596a23d
abhana <abhana@homechoice.co.za>
2021-05-05 09:55:53 +0200

    enums added

---
+++ TicTacToeAPI/TicTacToeAPI/Infrastructure/Models/enums/GamePlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace TicTacToeAPI.Infrastructure.Models.enums
+{
+  public enum GamePlayer
+  {
+    [EnumMember(Value = "Human")]
+    Human,
+    [EnumMember(Value = "Cpu")]
+    Cpu
+  }
+}
---
+++ TicTacToeAPI/TicTacToeAPI/Infrastructure/Models/enums/GameStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace TicTacToeAPI.Infrastructure.Models.enums
+{
+  public enum GameStatus
+  {
+    [EnumMember(Value = "Incomplete")]
+    Incomplete,
+    [EnumMember(Value = "PlayerOneWon")]
+    PlayerOneWon,
+    [EnumMember(Value = "PlayerTwoWon")]
+    PlayerTwoWon,
+    [EnumMember(Value = "Draw")]
+    Draw
+  }
+}
